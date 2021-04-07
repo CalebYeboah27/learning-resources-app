7825f38711786e3c45c72119025b28e7da26ec03
calebyeboah27 <calebyeboah27@gmail.com>
2021-04-07 12:37:45 +0100

    Initial commit

---
+++ src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <li>
+      <base-card>
+        <header>
+          <h3>{{ title }}</h3>
+          <button>Delete</button>
+        </header>
+        <p>{{ description }}</p>
+        <nav>
+          <a :href="link">View resource</a>
+        </nav>
+      </base-card>
+    </li>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  props: ['title', 'description', 'link']
+};
+</script>
+
+<style scoped>
+li {
+  margin: auto;
+  max-width: 40rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h3 {
+  font-size: 1.25rem;
+  margin: 0.5rem 0;
+}
+
+a {
+  text-decoration: none;
+  text-transform: capitalize;
+  color: coral;
+}
+</style>
